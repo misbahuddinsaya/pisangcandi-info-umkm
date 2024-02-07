ef437036eff98e27940d976f4e9fa49b153d1844
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-08 02:09:22 +0700

    first 7

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('title', 'Home')
 @section('content')
-<section class="breadcrumb-section-dash set-bg" data-setbg=" {{ asset('assets/img/breadcrumb-dashboard.jpg') }} ">
+<section class="breadcrumb-section-dash set-bg" data-setbg=" {{ asset('img/breadcrumb-dashboard.jpg') }} ">
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-12 text-center">
@@ -22,7 +22,7 @@
 </section>
 <section class="hero">
     <div class="container">
-        <div class="hero__item set-bg" data-setbg=" {{ asset('assets/img/hero/banner.jpg') }} ">
+        <div class="hero__item set-bg" data-setbg=" {{ asset('img/hero/banner.jpg') }} ">
             <div class="hero__text">
                 <span>UMKM SAYA</span>
                 <h2>UMKM RW. 04 <br />PISANG COKLAT</h2>
@@ -48,20 +48,4 @@
         </div>
     </div>
 </section>
-<div class="banner">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="banner__pic">
-                    <img src="img/banner/banner-1.jpg" alt="">
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-                <div class="banner__pic">
-                    <img src="img/banner/banner-2.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
