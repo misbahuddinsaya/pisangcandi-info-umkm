97ff05f02e3cabd6b620b6fd499ee9fdb82820a6
misbahuddinsaya <sayamisbahuddin26@gmail.com>
2024-02-08 23:40:56 +0700

    42

--- resources/views/partials/daftar-umkm.blade.php
+++ resources/views/partials/daftar-umkm.blade.php
@@ -3,10 +3,10 @@
 </div>
 <div class="container">
     <div class="row">
-        @foreach ($dataUmkm as $item)
+        @foreach ($dataUmkm as $umkm)
         <div class="col-lg-3">
-            <div class="categories__item set-bg" data-setbg="<?= $item['profile_umkm']; ?>">
-                <h5><a href="{{ route('umkm-detail', ['id' => $item['kode_umkm']]) }}">{{ $item['nama_umkm'] }}</a></h5>
+            <div class="categories__item set-bg" data-setbg="<?= $umkm['profile_umkm']; ?>">
+                <h5><a href="{{ route('umkm-detail', ['id' => $umkm['kode_umkm']]) }}">{{ $umkm['nama_umkm'] }}</a></h5>
             </div>
         </div>
         @endforeach
--- resources/views/partials/produk-umkm.blade.php
+++ resources/views/partials/produk-umkm.blade.php
@@ -1,7 +1,7 @@
 @foreach ($produkUmkm as $item)
 <div class="col-lg-3 col-md-6 col-sm-6">
     <div class="product__item">
-        <div class="product__item__pic set-bg" data-setbg="/Produk/<?= $item['foto_produk']; ?>">
+        <div class="product__item__pic set-bg" data-setbg="<?= $item['foto_produk']; ?>">
             <ul class="product__item__pic__hover">
                 <li><a href=""><i class="fa fa-info"></i></a></li>
             </ul>
