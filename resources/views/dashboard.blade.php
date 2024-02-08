3d2407e1df75dddb8cc43ec10335c5d04fbad79f
misbahuddinsaya <sayamisbahuddin26@gmail.com>
2024-02-08 22:24:40 +0700

    35

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,7 +25,7 @@
         <div class="hero__item set-bg" data-setbg=" /img/hero/banner.jpg">
             <div class="hero__text">
                 <span>UMKM SAYA</span>
-                <h2>UMKM RW. 04 <br />PISANG COKLAT</h2>
+                <h2>UMKM RW. 04 <br />PISANG CANDI</h2>
                 <a href="#" class="primary-btn">TEMUKAN</a>
             </div>
         </div>
