abfa1e63b4026ed15b3680a62acad0702f76ca36
misbahuddinsaya <sayamisbahuddin26@gmail.com>
2024-02-08 23:28:19 +0700

    39

--- resources/views/partials/daftar-umkm.blade.php
+++ resources/views/partials/daftar-umkm.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         @foreach ($dataUmkm as $item)
         <div class="col-lg-3">
-            <div class="categories__item set-bg" data-setbg="/Produk/<?= $item['profile_umkm']; ?>">
+            <div class="categories__item set-bg" data-setbg="<?= $item['profile_umkm']; ?>">
                 <h5><a href="{{ route('umkm-detail', ['id' => $item['kode_umkm']]) }}">{{ $item['nama_umkm'] }}</a></h5>
             </div>
         </div>
