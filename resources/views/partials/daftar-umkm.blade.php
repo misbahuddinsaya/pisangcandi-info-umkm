a8f21509d2db4a9db5e0ea57943a9492a28a0e0d
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-11 23:30:14 +0700

    coba1

--- resources/views/partials/daftar-umkm.blade.php
+++ resources/views/partials/daftar-umkm.blade.php
@@ -3,12 +3,18 @@
 </div>
 <div class="container">
     <div class="row">
+        @if ($dataUmkm && count($dataUmkm) > 0)
         @foreach ($dataUmkm as $umkm)
         <div class="col-lg-3">
-            <div class="categories__item set-bg" data-setbg="<?= $umkm['profile_umkm']; ?>">
+            <div class="categories__item set-bg" data-setbg="{{ $umkm['profile_umkm'] }}">
                 <h5><a href="{{ route('umkm-detail', ['id' => $umkm['kode_umkm']]) }}">{{ $umkm['nama_umkm'] }}</a></h5>
             </div>
         </div>
         @endforeach
+        @else
+        <div class="col-lg-12">
+            <p>Tidak ada UMKM yang tersedia saat ini.</p>
+        </div>
+        @endif
     </div>
 </div>
