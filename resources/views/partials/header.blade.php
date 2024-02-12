ac18b21857bec22c6620b2e36b57ac53433b51be
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-12 20:34:40 +0700

    c11

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -23,7 +23,6 @@
                     <li><a href="{{ route('admin-umkm')}}">Profil Admin</a></li>
                     @elseif(session('user_level') == 'pelaku-umkm')
                     <li><a href="{{ route('pelaku-umkm')}}">Profil UMKM</a></li>
-                    @elseif(session('user_level') == 'pelaku-umkm')
                     <li><a href="{{ route('daftar-umkm') }}">Daftar UMKM</a></li>
                     @endif
                     <li><a href="{{ route('logout-user')}}">Logout</a></li>
