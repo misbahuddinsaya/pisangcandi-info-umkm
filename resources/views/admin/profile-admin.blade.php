63f32096fd3c95c04b32697f2ac325e914353cde
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-12 11:14:46 +0700

    coba17

--- resources/views/admin/profile-admin.blade.php
+++ resources/views/admin/profile-admin.blade.php
@@ -20,42 +20,7 @@
 <section class="produk-add mt-5 mb-5">
     <div class="container-produk-add">
         <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-header primary-btn">Tambah UMKM</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <form action="{{route('simpan-umkm')}}" method="POST" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="mb-2">
-                                        <label for="namaUmkm" class="form-label">Nama UMKM</label>
-                                        <input type="text" class="form-control" id="namaUmkm" placeholder="Nama UMKM" name="namaUmkm" value="">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="alamat" class="form-label">Alamat</label>
-                                        <input type="text" class="form-control" id="alamat" placeholder="Alamat" name="alamat" value="">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="no_tlp" class="form-label">Nomor Telepon</label>
-                                        <input type="number" class="form-control" id="nomer_tlp" placeholder="Nomer Telepon" name="no_tlp" required value="">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="jumlah_produk" class="form-label">Jumlah Produk</label>
-                                        <input type="number" class="form-control" id="jumlah_produk" placeholder="Jumlah Produk" name="jumlah_produk" value="">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="formFile" class="form-label">Profile UMKM</label>
-                                        <input type="file" class="form-control" id="formFile" name="file" placeholder="Nama Produk" required value="">
-                                    </div>
-                                    <button type="submit" class="btn primary-btn">Tambah UMKM</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header primary-btn">Tambah User</div>
                     <div class="card-body">
