d22a1f103b622769b986f686669dcca34fc86f74
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-13 11:30:22 +0700

    c57

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
         $username = $request->input('username');
         $password = $request->input('password');
 
-        $referenceUser = $this->database->getReference('tb_user');
+        $referenceUser = $this->database->getReference('tb_userumkm');
         $dataUser = $referenceUser->getValue();
 
         if ($dataUser === null || empty($dataUser)) {
