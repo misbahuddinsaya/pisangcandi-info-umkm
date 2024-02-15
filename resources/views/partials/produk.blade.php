9fef5cff5191e18bc723497934d9b2e37d12d908
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-15 23:44:36 +0700

    Commit Didin

--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -1,16 +1,19 @@
 @if(isset($totalProduk) && $totalProduk > 0):
 @foreach ($dataProduk as $item)
-<div class="col-lg-3 col-md-2 col-sm-2">
-    <div class="card" style="width: 18rem;">
-        <img src="<?= $item['foto_produk']; ?>" class="product__item__pic set-bg" alt="">
-        <div class="card-body">
-            <h5 class="card-title">{{ $item['nama_produk'] }}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}" class="btn btn-primary">Go somewhere</a>
-        </div>
+<div class="col">
+    <div class="product__item">
+        <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">
+            <div class="product__item__pic set-bg" data-setbg="">
+                <ul class="product__item__pic__hover">
+                    <li><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}"><i class="fa fa-info"></i></a></li>
+                </ul>
+            </div>
+            <div class="product__item__text">
+                <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a>
+            </div>
+        </a>
     </div>
 </div>
-
 @endforeach
 @else
 <div class="col-12">
