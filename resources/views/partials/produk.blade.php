0229ffd4f88ac772d42aefc9339b8d6c09f0515f
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-15 23:29:43 +0700

    Commit Didin

--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -2,11 +2,13 @@
 @foreach ($dataProduk as $item)
 <div class="col-lg-3 col-md-2 col-sm-2">
     <div class="product__item">
-        <div class="product__item__pic set-bg" data-setbg="<?= $item['foto_produk']; ?>">
-            <ul class="product__item__pic__hover">
-                <li><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}"><i class="fa fa-info"></i></a></li>
-            </ul>
-        </div>
+        <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">
+            <div class="product__item__pic set-bg" data-setbg="<?= $item['foto_produk']; ?>">
+                <ul class="product__item__pic__hover">
+                    <li><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}"><i class="fa fa-info"></i></a></li>
+                </ul>
+            </div>
+        </a>
         <div class="product__item__text">
             <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a>
         </div>
