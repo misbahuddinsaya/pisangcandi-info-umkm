a1231e3b1645d21751509b5f332645f588bd1b61
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-16 10:17:41 +0700

    Commit Didin

--- resources/views/konsumen/produk-konsumen.blade.php
+++ resources/views/konsumen/produk-konsumen.blade.php
@@ -71,12 +71,7 @@
         <div class="row">
             @include('partials.produk')
         </div>
-        <div class="product__pagination">
-            <a href="#">1</a>
-            <a href="#">2</a>
-            <a href="#">3</a>
-            <a href="#"><i class="fa fa-long-arrow-right"></i></a>
-        </div>
+
 
 
     </div>
