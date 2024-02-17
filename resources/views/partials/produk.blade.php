17265b1ba4913cf9321787d53d18671225f1b046
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-17 21:38:53 +0700

    Commit Didin

--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -1,18 +1,11 @@
 @if(isset($totalProduk) && $totalProduk > 0):
 @foreach ($dataProduk as $item)
 <div class="col-lg-3 col-md-6 col-sm-6">
-    <div class="product__item">
-        <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">
-            <div class="product__item__pic set-bg" data-setbg="<?= $item['foto_produk']; ?>">
-                <ul class="product__item__pic__hover">
-                    <li><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}"><i class="fa fa-info"></i></a></li>
-                </ul>
-            </div>
-            <div class="product__item__text">
-                <a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a>
-            </div>
-        </a>
+
+    <div class="categories__item set-bg" data-setbg="<?= $item['foto_produk']; ?>">
+        <h5><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a></h5>
     </div>
+
 </div>
 @endforeach
 @else
