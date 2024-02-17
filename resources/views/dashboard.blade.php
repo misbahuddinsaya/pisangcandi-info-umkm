c5c6270a74a1070412ecccbec372e71b9af3c2d9
misbahuddinsaya <misbahuddinsaya26@gmail.com>
2024-02-17 22:05:34 +0700

    Commit Didin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,11 +35,11 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="section-title">
-                    <h2>Produk Terbaru</h2>
+                    <h2>Produk UMKM</h2>
                 </div>
             </div>
         </div>
-        <div class="row featured__filter">
+        <div class="row">
             @include('partials.produk')
         </div>
     </div>
--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -1,8 +1,10 @@
 @if(isset($totalProduk) && $totalProduk > 0):
 @foreach ($dataProduk as $item)
-<div class="col-lg-4 col-md-6 col-sm-3 col-12">
-    <div class="categories__item set-bg" data-setbg="<?= $item['foto_produk']; ?>">
-        <h5><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a></h5>
+<div class="categories__slider owl-carousel">
+    <div class="col-lg-3">
+        <div class="categories__item set-bg" data-setbg="<?= $item['foto_produk']; ?>">
+            <h5><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a></h5>
+        </div>
     </div>
 </div>
 @endforeach
