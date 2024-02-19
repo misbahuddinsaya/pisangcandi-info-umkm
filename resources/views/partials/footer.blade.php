4ab010404b04da1ca81f06935304d99868bd0b63
misbahuddin321 <xyazustres@gmail.com>
2024-02-19 09:26:27 +0700

    commit didin

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,12 +8,12 @@
                         <h3>Pisang Candi</h3>
                     </div>
                     <div class="row">
-                        <div class="col">
+                        <div class="col mb-4">
                         <div class="logo-umkm" style="max-width: 100px;">
                     <a href="/"><img src="/img/Logo_Unmer_resmi.png" alt=""></a>
                 </div>
                         </div>
-                        <div class="col">
+                        <div class="col mb-4">
                         <div class="logo-umkm" style="max-width: 106px;">
                     <a href="/"><img src="/img/logoumer.png" alt=""></a>
                 </div>
@@ -21,13 +21,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 offset-lg-1">
-
+            <div class="col-lg-3 col-md-3 col-sm-3 offset-lg-1 mb-4">
             </div>
             <div class="col-lg-4 col-md-12">
                 <div class="footer__widget">
                     <h6>&copy; 2024 UMKM RW 04 Pisang Candi.</h6>
-                    <p>info@umkm-rw04-pisangcandi.com</p>
+                    <p>pisangcandi-infoumkm.vercel.app</p>
                     <div class="footer__widget__social">
                         <a href="#"><i class="fa fa-facebook"></i></a>
                         <a href="#"><i class="fa fa-instagram"></i></a>
