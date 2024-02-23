0f5b161e5809f2ee4376180cf2d2d180bd6540f9
misbahuddin321 <xyazustres@gmail.com>
2024-02-23 11:01:32 +0700

    commit didin

--- resources/views/admin/edit-admin.blade.php
+++ resources/views/admin/edit-admin.blade.php
@@ -26,7 +26,7 @@
                     <div class="card-body">
                         <form class="row g-3 needs-validation" novalidate action="https://pisangcandi-umkminfo.vercel.app/profile-admin/update-umkm/{{ $productData['kode_produk'] }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            @method('PUT')
+                            @method('POST')
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="mb-2">
