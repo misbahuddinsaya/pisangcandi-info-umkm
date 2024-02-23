fe625a6c27479cc7a06ac7a9c90df946b12292c7
misbahuddin321 <xyazustres@gmail.com>
2024-02-23 11:16:27 +0700

    commit didin

--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -1,7 +1,7 @@
 @if(isset($totalProduk) && $totalProduk > 0):
 @foreach ($dataProduk as $item)
 <div class="col-lg-3 mb-5">
-    <div class="categories__item set-bg" data-setbg="<?= $item['foto_produk2']; ?>">
+    <div class="categories__item set-bg" data-setbg="<?= $item['foto_produk1']; ?>">
         <h5><a href="{{ route('produk-info', ['id' => $item['kode_produk']]) }}">{{ $item['nama_produk'] }}</a></h5>
     </div>
 </div>
