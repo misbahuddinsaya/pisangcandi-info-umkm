bd39c8a007b6cafd8d3ad37bb26f5e00ddfc4016
misbahuddin321 <xyazustres@gmail.com>
2024-02-23 11:38:20 +0700

    commit didin

--- resources/views/partials/produk.blade.php
+++ resources/views/partials/produk.blade.php
@@ -1,13 +1,13 @@
-@if(isset($totalProduk) && $totalProduk > 0):
-@foreach ($dataProduk as $produk)
-<div class="col-lg-3 mb-5">
-    <div class="categories__item set-bg" data-setbg="">
-        <h5><a href="">{{ $produk['nama_produk'] }}</a></h5>
-    </div>
-</div>
-@endforeach
+@if(isset($totalProduk) && $totalProduk > 0)
+    @foreach ($dataProduk as $produk)
+        <div class="col-lg-3 mb-5">
+            <div class="categories__item set-bg" data-setbg="{{ $produk['foto_produk1'] }}">
+                <h5><a href="">{{ $produk['nama_produk'] }}</a></h5>
+            </div>
+        </div>
+    @endforeach
 @else
-<div class="col-12">
-    <p class="text-center">Tidak Ada Produk UMKM</p>
-</div>
+    <div class="col-12">
+        <p class="text-center">Tidak Ada Produk UMKM</p>
+    </div>
 @endif
